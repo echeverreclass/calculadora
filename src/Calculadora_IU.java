639c8b8eb573c1cde7ab093df25c8c7f159bb0f9
leydi <lecheverrec@isanagustin.edu.pe>
2023-09-08 19:21:46 -0500

    agregue codigo al boton dividir

--- src/Calculadora_IU.java
+++ src/Calculadora_IU.java
@@ -69,6 +69,11 @@ public class Calculadora_IU extends javax.swing.JFrame {
         });
 
         btnDividir.setText("DIVIDIR");
+        btnDividir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDividirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -154,6 +159,14 @@ public class Calculadora_IU extends javax.swing.JFrame {
         double multiplicar =nro1+nro2;
         JOptionPane.showMessageDialog(this, "La multiplicacion es: "+multiplicar);
     }//GEN-LAST:event_btnMultiplicarActionPerformed
+
+    private void btnDividirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDividirActionPerformed
+        // TODO add your handling code here:
+        double nro1=Double.parseDouble(txtNro1.getText());
+        double nro2=Double.parseDouble(txtNro2.getText());
+        double dividir =nro1+nro2;
+        JOptionPane.showMessageDialog(this, "La divicion es: "+dividir);
+    }//GEN-LAST:event_btnDividirActionPerformed
 
     /**
      * @param args the command line arguments
